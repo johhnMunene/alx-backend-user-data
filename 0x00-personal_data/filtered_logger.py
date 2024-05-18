7031aae9968c06ddd4537ff2d8a51af309b8bfcd
johhnMunene <Edwardsjohnny64@gmail.com>
2024-05-18 11:46:37 -0500

    ALX_BACKEND_USER_DATAALX_BACKEND_USER_DATA

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+ #!/usr/bin/env python3
+ """function filter_datum:
+    Arguments
+        fields
+        redaction
+        message
+        separator
+"""
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    pattern = r'(' + '|'.join(fields) + r')=[^' + separator + r']*'
+    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
+
+
